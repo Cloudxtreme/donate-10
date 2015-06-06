0eafe23f67dedb79c59df397d78e332e08ca07ea
Allan Kenneth Ang <allkenang@gmail.com>
2015-06-06 11:40:48 +0800

    added missing files

---
+++ core/src/main/java/com/djavafactory/service/impl/RoleManagerImpl.java
@@ -0,0 +1,53 @@
+package com.djavafactory.service.impl;
+
+import com.djavafactory.dao.RoleDao;
+import com.djavafactory.model.Role;
+import com.djavafactory.service.RoleManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Implementation of RoleManager interface.
+ *
+ * @author <a href="mailto:dan@getrolling.com">Dan Kibler</a>
+ */
+@Service("roleManager")
+public class RoleManagerImpl extends GenericManagerImpl<Role, Long> implements RoleManager {
+    RoleDao roleDao;
+
+    @Autowired
+    public RoleManagerImpl(RoleDao roleDao) {
+        super(roleDao);
+        this.roleDao = roleDao;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<Role> getRoles(Role role) {
+        return dao.getAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role getRole(String rolename) {
+        return roleDao.getRoleByName(rolename);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Role saveRole(Role role) {
+        return dao.save(role);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void removeRole(String rolename) {
+        roleDao.removeRole(rolename);
+    }
+}
