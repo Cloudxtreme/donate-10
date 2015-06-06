5f38a8eb006b0a4f2d37ac64097f8de85e156281
Allan Kenneth Ang <allkenang@gmail.com>
2015-06-06 16:35:30 +0800

    partial facebook auto post to page

---
+++ core/src/main/java/com/djavafactory/service/SocialPostingManager.java
@@ -0,0 +1,12 @@
+package com.djavafactory.service;
+
+/**
+ *
+ * Posts to various official social networks
+ * @author aka
+ * @date: 06 June 2015
+ */
+public interface SocialPostingManager {
+
+    void post(String message);
+}
